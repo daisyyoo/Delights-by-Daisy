21b0c3af34c1269ed2453eb2ee67510bc78de6cb
Daisy Yoo <daisyhyoo@gmail.com>
2022-11-07 23:02:07 +0000

    Imported database schema and writing cookie data

---
+++ database/data.sql
@@ -0,0 +1,18 @@
+insert into "cookies" (
+  "flavor",
+  "price",
+  "weight",
+  "description",
+  "ingredients",
+  "allergens",
+  "backstory"
+  )
+values (
+  'chocolate chip',
+  '400',
+  '3.5',
+  'This is my ideal chocolate chip cookie: crunchy edges, chewy center and lots of high quality chocolate with just enough sweetness to enjoy the cookie.',
+  'flour, granulated sugar, brown sugar, eggs, butter, vanilla, salt, baking soda, baking powder, dark chocolate, milk chocolate',
+  'gluten, dairy, eggs',
+  'During covid, I had a lot of time on my hands and craved chocolate chip cookies. I am very picky when it comes to chocolate cookies though. Many sold commercially are usually too sweet that it gives me a headache and I cannot finish the whole cookie. So with ample time and no place to go, I decided to make my own recipe. With lots of high quality chocolate and plenty of resting time, this is my version of what a chocolate chip cookie should be. I like to call it lots of high quality chocolate with a side of cookie.'
+  );
