996e5838db4232a74c3cdd9ed9e9884c5f8c2f56
Daisy Yoo <daisyhyoo@gmail.com>
2022-11-09 23:04:21 +0000

    Figured out styling for most of page, need to style input for qty and add to basket button

--- client/pages/products.jsx
+++ client/pages/products.jsx
@@ -4,6 +4,32 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
 
+const styles = {
+  title: {
+    color: '#422300',
+    fontWeight: '600'
+  },
+  image: {
+    height: '300px',
+    objectFit: 'cover'
+  },
+  description: {
+    color: '#693802',
+    fontSize: '1rem',
+    lineHeight: '1.3rem'
+  },
+  weight: {
+    color: '#693802',
+    fontSize: '0.8rem'
+  },
+  price: {
+    color: '#693802',
+    fontWeight: '600'
+  },
+  accordionText: {
+    color: '#422300'
+  }
+};
 export default class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
@@ -26,36 +52,36 @@ export default class ProductDetails extends React.Component {
     return (
       <>
         <div>
-          <h1>{flavor}</h1>
-          <p>{`${weight} oz`}</p>
+          <h1 style={styles.title}>{flavor}</h1>
+          <p style={styles.weight}>{`${weight} oz`}</p>
         </div>
         <Card>
-          <Card.Img variant="top" src={imageUrl} alt={flavor} />
+          <Card.Img variant="top" src={imageUrl} alt={flavor} style={styles.image} />
           <Card.Body>
-            <Card.Text>
+            <Card.Text style={styles.description}>
               {description}
             </Card.Text>
           </Card.Body>
-          <Card.Body>
+          <Card.Body className="py-0" style={styles.price}>
             {toDollars(price)}
           </Card.Body>
-          <Card.Body>
+          <Card.Body className="d-flex justify-content-between">
             <input type="number" />
-            <Button>Go To Basket</Button>
+            <Button className="button-all">ADD TO BASKET</Button>
           </Card.Body>
         </Card>
-        <Accordion>
+        <Accordion className="my-4">
           <Accordion.Item eventKey="0">
             <Accordion.Header>Ingredients</Accordion.Header>
-            <Accordion.Body>{ingredients}</Accordion.Body>
+            <Accordion.Body style={styles.accordionText}>{ingredients}</Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header>Allergens</Accordion.Header>
-            <Accordion.Body>{allergens}</Accordion.Body>
+            <Accordion.Body style={styles.accordionText}>{allergens}</Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
             <Accordion.Header>Backstory</Accordion.Header>
-            <Accordion.Body>{backstory}</Accordion.Body>
+            <Accordion.Body style={styles.accordionText}>{backstory}</Accordion.Body>
           </Accordion.Item>
         </Accordion>
       </>
