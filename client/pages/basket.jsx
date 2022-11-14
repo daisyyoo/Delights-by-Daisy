28660f7dfdeacf7928bee8b246d8b2e39c092998
Daisy Yoo <daisyhyoo@gmail.com>
2022-11-14 23:05:42 +0000

    Still styling

--- client/pages/basket.jsx
+++ client/pages/basket.jsx
@@ -7,8 +7,11 @@ const styles = {
   image: {
     objectFit: 'contain'
   },
+  imageContainer: {
+    height: '180px'
+  },
   card: {
-    height: '150px'
+    height: '180px'
   },
   title: {
     color: '#422300',
@@ -28,6 +31,23 @@ const styles = {
   weight: {
     color: '#693802',
     fontSize: '0.8rem'
+  },
+  subtotalHeader: {
+    fontWeight: '600',
+    fontSize: '1rem'
+  },
+  subtotalText: {
+    color: '#693802',
+    fontSize: '0.8rem'
+  },
+  borderTop: {
+    borderTop: 'solid 2px #94540F'
+  },
+  borderBottom: {
+    borderBottom: 'solid 2px #94540F'
+  },
+  button: {
+    fontSize: '1rem'
   }
 };
 export default class Basket extends React.Component {
@@ -60,29 +80,31 @@ export default class Basket extends React.Component {
       <>
         <h1 className="py-1" style={styles.header}>My Basket</h1>
         <p className="m-0" style={styles.text}>{`${this.state.cookies.length} items`}</p>
-        <div className="d-lg-flex">
-          <div className="row">
+        <div className="d-lg-flex justify-content-lg-between container">
+          <div className="row col-lg-8">
             {/* if statement if !token and populate a message */}
             {
             this.state.cookies.map((product, index) => (
-              <div key={index} className="card-container d-flex basket-border py-3">
+              <div key={index} className="d-flex py-lg-3 justify-content-lg-start">
                 <BasketItems product={product} />
               </div>
             ))
           }
           </div>
-          <div className="d-flex flex-column align-items-center my-3" >
-            <div className="d-flex w-100 justify-content-between">
-              <h4>{`Subtotal (${this.state.cookies.length} items)`}</h4>
-              <h4>
+          <div className="py-3 col-lg-3" >
+            <div className="d-flex w-100 justify-content-between pt-3" style={styles.borderTop}>
+              <h5 style={styles.subtotalHeader}>{`Subtotal (${this.state.cookies.length} items)`}</h5>
+              <h5 style={styles.subtotalHeader}>
                 {toDollars(
                   this.state.cookies.reduce((previousCookie, currentCookie) => {
                     return previousCookie + (currentCookie.quantity * currentCookie.price);
                   }, 0))}
-              </h4>
+              </h5>
             </div>
-            <p>Taxes and shipping calculated at checkout</p>
-            <Button className="button-all w-75 w-lg-50">PROCEED TO CHECKOUT</Button>
+            <div style={styles.borderBottom} className="w-100 pb-4 d-flex flex-column align-items-center">
+              <p style={styles.subtotalText} className="w-100 text-left py-lg-2">Taxes and shipping calculated at checkout</p>
+              <Button style={styles.button} className="button-all w-100" >PROCEED TO CHECKOUT</Button>
+            </div>
           </div>
         </div>
 
@@ -96,11 +118,11 @@ function BasketItems(props) {
   // cartId, cookieId
   return (
     <>
-      <div className="col-5 d-flex align-items-center">
-        <img className="w-100" style={styles.image} src={imageUrl} alt={flavor}/>
+      <div style={styles.imageContainer} className="col-5 d-flex align-items-center basket-border">
+        <img className="h-100 img-fluid rounded" style={styles.image} src={imageUrl} alt={flavor}/>
       </div>
-      <Card className="col-7 border-0">
-        <Card.Body>
+      <Card className="col-7 card-border" style={styles.card}>
+        <Card.Body className="d-flex flex-column justify-content-between">
           <Card.Text className="m-0" style={styles.title}>{flavor}</Card.Text>
           <Card.Text className="m-0" style={styles.weight}>{`${weight} oz`}</Card.Text>
           <Card.Text className="m-0" style={styles.price}>{`${toDollars(price)}`}</Card.Text>
