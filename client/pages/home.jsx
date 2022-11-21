0a2b624433fd57663aa1790f2ffd3e444f01f8d5
Daisy Yoo <daisyhyoo@gmail.com>
2022-11-21 22:02:43 +0000

    Working on home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const styles = {
+  text: {
+    position: 'absolute',
+    left: '100px',
+    top: '150px'
+  }
+};
+
 export default function Home(props) {
+
   return (
-    <div>
-      Hello
+    <div className="row">
+      <img className="img-fluid" src="/image/home-pic.jpg" alt="plate-of-cookies"/>
+      <h1 style={styles.text}>Welcome to the homepage!</h1>
     </div>
   );
 }
