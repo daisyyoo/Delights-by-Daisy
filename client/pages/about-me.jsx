14e4580718984cc69f5de7fc3ab760c33747dd33
Daisy Yoo <daisyhyoo@gmail.com>
2022-12-03 05:42:47 +0000

    Finished desktop layout for about me page

--- client/pages/about-me.jsx
+++ client/pages/about-me.jsx
@@ -13,8 +13,7 @@ const styles = {
     color: '#422300'
   },
   background: {
-    backgroundColor: '#FFD8A9',
-    height: '50vh'
+    backgroundColor: '#FFD8A9'
   },
   imageContainer: {
     position: 'relative',
@@ -22,34 +21,41 @@ const styles = {
   },
   image: {
     position: 'absolute',
-    height: '400px',
-    width: '75vw',
-    objectFit: 'cover',
-    padding: '0'
+    padding: '0',
+    objectFit: 'cover'
+  },
+  height: {
+    height: '100%'
   }
 };
 
 export default class AboutMe extends React.Component {
   render() {
     return (
-      <div className="container mt-3">
-        <div className="row flex-column">
-          <h1>About Me</h1>
-          <h4 className="mt-3" style={styles.header}>Backstory</h4>
-          <hr style={styles.line}/>
-          <h6 style={styles.text} className="pb-4 px-3">
-            Hi! I&apos;m Daisy, and I love food. I love food so much I wanted to make a career out of it and went to The Culinary Institue of America for cooking, not baking. I don’t have a sweet tooth like most. But when I decide to indulge in something sweet, my favorite is a chocolate chip cookie.
-          </h6>
+      <>
+        <div className="container mt-3">
+          <div className="row flex-column">
+            <h1>About Me</h1>
+            <h4 className="mt-3" style={styles.header}>Backstory</h4>
+            <hr style={styles.line}/>
+            <h6 style={styles.text} className="pb-2">
+              Hi! I&apos;m Daisy, and I love food. I love food so much I wanted to make a career out of it and went to The Culinary Institue of America for cooking, not baking. I don’t have a sweet tooth like most. But when I decide to indulge in something sweet, my favorite is a chocolate chip cookie.
+            </h6>
+          </div>
         </div>
-        <div className="row justify-content-end" style={styles.imageContainer}>
-          <img style={styles.image} src="/image/about-me.webp" alt="lots-of-cookies"/>
+        <div className="row justify-content-end m-0" style={styles.imageContainer}>
+          <img style={styles.image} className="about-me-image" src="/image/about-me.webp" alt="lots-of-cookies"/>
         </div>
-        <div className="row flex-column justify-content-end pt-5" style={styles.background}>
-          <h6 style={styles.text} className="p-3">
-            As you can assume, I am very picky about my cookie. I never found one I loved, not too sweet, crunchy edges, chewy center, LOTS of good quality chocolate and all those beautiful cracks that add beauty and texture. So during covid, I decided to make my own version. After perfecting what I think is the perfect chocolate chip cookie, I made different flavors and variations. I hope my cookie research gives you a moment of delight.
-          </h6>
+        <div style={styles.background} className="about-me-background">
+          <div className="container" style={styles.height}>
+            <div style={styles.height} className="d-flex flex-column justify-content-end">
+              <h6 style={styles.text} className="py-3">
+                As you can assume, I am very picky about my cookie. I never found one I loved, not too sweet, crunchy edges, chewy center, LOTS of good quality chocolate and all those beautiful cracks that add beauty and texture. So during covid, I decided to make my own version. After perfecting what I think is the perfect chocolate chip cookie, I made different flavors and variations. I hope my cookie research gives you a moment of delight.
+              </h6>
+            </div>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
