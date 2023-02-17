d8a3cb323cb618309a11b237e2f9c0682a718533
Daisy Yoo <daisyhyoo@gmail.com>
2023-02-17 06:04:23 +0000

    Changed catalog page to react hooks. Page rendering and functional

--- client/pages/catalog.jsx
+++ client/pages/catalog.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { toDollars } from '../lib/';
 import Card from 'react-bootstrap/Card';
 
@@ -30,71 +30,61 @@ const styles = {
   }
 };
 
-export default class Catalog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cookies: [],
-      loading: true,
-      error: false
-    };
-  }
+export default function Catalog() {
+  const [cookies, setCookies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('/cookies')
       .then(res => {
         if (res.status === 500) {
-          this.setState({ error: true });
+          setError(true);
         }
         return res.json();
       })
       .then(cookies => {
-        this.setState({ loading: false });
-        this.setState({ cookies });
+        setCookies(cookies);
+        setLoading(false);
       })
       .catch(err => console.error(err));
-  }
+  }, []);
 
-  render() {
-    const { loading, error } = this.state;
-    if (error) {
-      return (
+  return (
+    <>
+      {error === true && (
         <div style={styles.errorContent} className="my-5 text-center d-flex flex-column justify-content-center align-items-center">
           <h1 className="w-75">There was an error with the connection. Please try again.</h1>
           <img src="/image/sad-cookie.png" alt="sad-cookie"/>
         </div>
-      );
-    }
-    return (
-      <>
-        <div className="container mt-3">
-          <h1 className="py-1">Shop All</h1>
-          <div className="d-flex justify-content-between">
-            <a style={styles.description} className="text-decoration-none" href='#'>
-              <i className="fa-solid fa-chevron-left" style={styles.icon}/>
-              {' Home'}
-            </a>
-            <p style={styles.description}>{`${this.state.cookies.length} items`}</p>
-          </div>
-          <div className="row">
-            {
-            this.state.cookies.map(product => (
+      )}
+      <div className="container mt-3">
+        <h1 className="py-1">Shop All</h1>
+        <div className="d-flex justify-content-between">
+          <a style={styles.description} className="text-decoration-none" href='#'>
+            <i className="fa-solid fa-chevron-left" style={styles.icon}/>
+            {' Home'}
+          </a>
+          <p style={styles.description}>{`${cookies.length} items`}</p>
+        </div>
+        <div className="row">
+          {
+            cookies.map(product => (
               <div key={product.cookieId} className="col-6 col-lg-4">
                 <Product product={product} />
               </div>
             ))
           }
-          </div>
         </div>
-        {loading === true &&
-          <div className="loader d-flex justify-content-center align-items-center" />
+      </div>
+      {loading === true &&
+      <div className="loader d-flex justify-content-center align-items-center" />
         }
-        {loading === false &&
-          <div className="loader-hide" />
+      {loading === false &&
+      <div className="loader-hide" />
         }
-      </>
-    );
-  }
+    </>
+  );
 }
 
 function Product(props) {
