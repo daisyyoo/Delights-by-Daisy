51bbca88a1422fa9184c48494da630461b31baa8
Daisy Yoo <daisyhyoo@gmail.com>
2023-02-17 05:24:23 +0000

    Changed header to function from class component and removed commented code in page-container

--- client/components/page-container.jsx
+++ client/components/page-container.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function PageContainer({ children }) {
   return (
     <div id="page-container">
-      {/* <div className="container"> */}
       { children }
-      {/* </div> */}
     </div>
   );
 }
