3cbb9fd0b0b9de0b933c1078688a57851baa838b
Daisy Yoo <daisyhyoo@gmail.com>
2023-03-17 18:35:01 +0000

    Added another try to nest adding jwt token to local storage until after response.json was received

--- server/index.js
+++ server/index.js
@@ -70,7 +70,7 @@ app.post('/api/addToBasket', async (req, res, next) => {
       `;
       const result = await db.query(sql);
       cartId = result.rows[0].cartId;
-      jwt.sign({ cartId }, process.env.TOKEN_SECRET, (err, asyncToken) => {
+      await jwt.sign({ cartId }, process.env.TOKEN_SECRET, (err, asyncToken) => {
         if (err) { console.error(err); }
         token = asyncToken;
       });
